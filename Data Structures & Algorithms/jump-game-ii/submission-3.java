82c40ea0cebf8571795d9457e82ead24365412f4
AnishaBansal410 <62749801+AnishaBansal410@users.noreply.github.com>
2026-06-13 08:13:10 +0530

    Add: jump-game-ii - submission-3

---
+++ Data Structures & Algorithms/jump-game-ii/submission-3.java
@@ -0,0 +1,26 @@
+class Solution {
+    public int jump(int[] nums) {
+        if(nums.length==1){
+            return 0;
+        }
+        int jumps=0;
+        int i=0;
+        int currEnd=nums[0];
+        int farthest=0;
+        while(i<nums.length){
+            farthest=Math.max(nums[i]+i,farthest);
+            if(i==currEnd){
+                currEnd=farthest;
+                jumps++;
+            }
+            if(currEnd>=nums.length){
+                break;
+            }
+            i++;
+        }
+        if(i<nums.length-1){
+            jumps++;
+        }
+        return jumps;
+    }
+}
