13ce4a0567ed447544fde9d7d15fb4f56a48d321
AnishaBansal410 <62749801+AnishaBansal410@users.noreply.github.com>
2026-06-15 14:53:08 +0530

    Add: permutations-ii - submission-0

---
+++ Data Structures & Algorithms/permutations-ii/submission-0.java
@@ -0,0 +1,30 @@
+class Solution {
+    public List<List<Integer>> permuteUnique(int[] nums) {
+        List<List<Integer>> ans = new ArrayList<>();
+        Arrays.sort(nums);
+        boolean[] idx = new boolean[nums.length];
+        backtrack(nums,idx,new ArrayList<>(),ans);
+        return ans;
+    }
+
+    public void backtrack(int[] nums,boolean[] idx,List<Integer> tmp,List<List<Integer>> ans){
+        if(tmp.size()==nums.length){
+            ans.add(new ArrayList<>(tmp));
+            return;
+        }
+        for(int i=0;i<nums.length;i++){
+            if(idx[i]){
+                continue;
+            }
+            if(i>0 && nums[i-1]==nums[i] && !idx[i-1]){
+                continue;
+            }
+            tmp.add(nums[i]);
+            idx[i]=true;
+            backtrack(nums,idx,tmp,ans);
+            tmp.remove(tmp.size()-1);
+            idx[i]=false;
+            
+        }
+    }
+}
