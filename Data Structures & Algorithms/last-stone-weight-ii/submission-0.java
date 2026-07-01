c240a8f5d8f9678652785e43b721a949137ab877
AnishaBansal410 <62749801+AnishaBansal410@users.noreply.github.com>
2026-07-01 14:36:44 +0530

    Add: last-stone-weight-ii - submission-0

---
+++ Data Structures & Algorithms/last-stone-weight-ii/submission-0.java
@@ -0,0 +1,24 @@
+class Solution {
+    public int lastStoneWeightII(int[] stones) {
+        int sum=0;
+        for(int i:stones){
+            sum+=i;
+        }
+        int stonesum=sum;
+        sum=sum/2;
+        int[][] dp = new int[stones.length+1][sum+1];
+        
+        for(int i=1;i<stones.length+1;i++){
+            for(int j=0;j<sum+1;j++){
+                if(stones[i-1]>j){
+                    dp[i][j]=dp[i-1][j];
+                }
+                else{
+                    dp[i][j]=Math.max(stones[i-1]+dp[i-1][j-stones[i-1]],dp[i-1][j]);
+                }
+            }
+        }
+
+        return stonesum-2*dp[stones.length][sum];
+    }
+}
