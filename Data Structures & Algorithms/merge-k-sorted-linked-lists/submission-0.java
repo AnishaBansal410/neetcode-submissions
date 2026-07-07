861b7bd7f83e1f3deab2f8ccd4538fcf7a405199
AnishaBansal410 <62749801+AnishaBansal410@users.noreply.github.com>
2026-07-07 13:42:21 +0530

    Add: merge-k-sorted-linked-lists - submission-0

---
+++ Data Structures & Algorithms/merge-k-sorted-linked-lists/submission-0.java
@@ -0,0 +1,59 @@
+/**
+ * Definition for singly-linked list.
+ * public class ListNode {
+ *     int val;
+ *     ListNode next;
+ *     ListNode() {}
+ *     ListNode(int val) { this.val = val; }
+ *     ListNode(int val, ListNode next) { this.val = val; this.next = next; }
+ * }
+ */
+
+class Solution {
+    public ListNode mergeKLists(ListNode[] lists) {
+        if(lists.length==0){
+            return null;
+        }
+        if(lists.length==1){
+            return lists[0];
+        }
+        ListNode ans = mergeTwoLists(lists[0],lists[1]);
+        int i=2;
+        while(i<lists.length){
+            ans=mergeTwoLists(ans,lists[i]);
+            i++;
+        }
+        return ans;
+    }
+
+    public ListNode mergeTwoLists(ListNode l1, ListNode l2) {
+        ListNode dummy = new ListNode(0);
+        ListNode head=dummy;
+        while(l1!=null && l2!=null){
+            if(l1.val<l2.val){
+                ListNode tmp = new ListNode(l1.val);
+                head.next=tmp;
+                l1=l1.next;
+            }
+            else{
+                ListNode tmp = new ListNode(l2.val);
+                head.next=tmp;       
+                l2=l2.next;   
+            }
+            head=head.next;
+        }
+        while(l1!=null){
+            ListNode tmp = new ListNode(l1.val);
+            head.next=tmp;
+            head=head.next;
+            l1=l1.next;
+        }
+        while(l2!=null){
+            ListNode tmp = new ListNode(l2.val);
+            head.next=tmp;
+            head=head.next;
+            l2=l2.next;
+        }
+        return dummy.next;
+    }
+}
