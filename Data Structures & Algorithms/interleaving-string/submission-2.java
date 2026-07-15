3096167ff2fb854fb0723d1a72412777abb1b456
AnishaBansal410 <62749801+AnishaBansal410@users.noreply.github.com>
2026-07-15 12:40:15 +0530

    Add: interleaving-string - submission-2

---
+++ Data Structures & Algorithms/interleaving-string/submission-2.java
@@ -0,0 +1,33 @@
+class Solution {
+    public boolean isInterleave(String s1, String s2, String s3) {
+        if(s1.length()+s2.length() != s3.length()){
+            return false;
+        }
+        boolean[][] dp = new boolean[s1.length()+1][s2.length()+1];
+        dp[0][0]=true;
+        for(int i=1;i<s1.length()+1;i++){
+            if(s1.charAt(i-1)==s3.charAt(i-1))
+                dp[i][0]=dp[i-1][0];
+        }
+        for(int i=1;i<s2.length()+1;i++){
+            if(s2.charAt(i-1)==s3.charAt(i-1))
+                dp[0][i]=dp[0][i-1];
+        }
+
+        for(int i=1;i<s1.length()+1;i++){
+            for(int j=1;j<s2.length()+1;j++){
+                if(s1.charAt(i-1)==s3.charAt(i+j-1) && s2.charAt(j-1)==s3.charAt(i+j-1)){
+                    dp[i][j] = dp[i-1][j] || dp[i][j-1];
+                }
+                else if(s1.charAt(i-1)==s3.charAt(i+j-1)){
+                    dp[i][j] = dp[i-1][j];
+                }
+                else if(s2.charAt(j-1)==s3.charAt(i+j-1)){
+                    dp[i][j]=dp[i][j-1];
+                }
+            }
+        }
+
+        return dp[s1.length()][s2.length()];
+    }
+}
