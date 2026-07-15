8856b9d5b91e16265f432dcc576564699cf8e062
AnishaBansal410 <62749801+AnishaBansal410@users.noreply.github.com>
2026-07-15 13:11:58 +0530

    Add: combination-sum-iv - submission-3

---
+++ Data Structures & Algorithms/combination-sum-iv/submission-3.java
@@ -0,0 +1,25 @@
+class Solution {
+    // int ans = 0;
+    public int combinationSum4(int[] nums, int target) {
+        int[] dp = new int[target+1];
+        dp[0]=1;
+        Arrays.sort(nums);
+        combinations(nums,target,dp);
+        return dp[target];
+    }
+
+    public int combinations(int[] nums,int target,int[] dp){
+        if(dp[target]!=0){
+            return dp[target];
+        }
+        int ans=0;
+        for(int i=0;i<nums.length;i++){
+            if(nums[i]>target){
+                break;
+            }
+            ans+=combinations(nums,target-nums[i],dp);
+        }
+        dp[target]=ans;
+        return ans;
+    }
+}
