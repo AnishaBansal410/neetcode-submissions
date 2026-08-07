bbd240c1a32635aea7b927931f88e2e6899bdbb7
AnishaBansal410 <62749801+AnishaBansal410@users.noreply.github.com>
2026-08-07 11:27:52 +0530

    Add: count-connected-components - submission-0

---
+++ Data Structures & Algorithms/count-connected-components/submission-0.java
@@ -0,0 +1,45 @@
+class Solution {
+    public int countComponents(int n, int[][] edges) {
+        List<Integer>[] adj = new ArrayList[n];
+        for(int i=0;i<n;i++){
+            adj[i]=new ArrayList<>();
+        }
+
+        for(int[] edge:edges){
+            adj[edge[0]].add(edge[1]);
+            adj[edge[1]].add(edge[0]);
+        }
+
+        Queue<Node> q = new ArrayDeque<>();
+        boolean[] visited = new boolean[n];
+        int ans = 0;
+
+        for(int i=0;i<n;i++){
+            if(!visited[i]){
+                q.offer(new Node(i,-1));
+                visited[i]=true;
+                while(!q.isEmpty()){
+                    Node curr = q.poll();
+                    for(int next : adj[curr.vertex]){
+                        if(!visited[next]){
+                            visited[next]=true;
+                            q.offer(new Node(next,curr.vertex));
+                        }
+                    }
+                }
+                ans++;
+            }
+        }
+        return ans;
+    }
+}
+
+class Node{
+    int vertex;
+    int parent;
+
+    public Node(int v,int p){
+        this.vertex = v;
+        this.parent = p;
+    }
+}
