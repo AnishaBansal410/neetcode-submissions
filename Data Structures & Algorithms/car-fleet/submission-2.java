8956a2062fd36e277b3c8a24ea8e0a02ed5e3bcf
AnishaBansal410 <62749801+AnishaBansal410@users.noreply.github.com>
2026-09-25 16:35:16 +0530

    Add: car-fleet - submission-2

---
+++ Data Structures & Algorithms/car-fleet/submission-2.java
@@ -0,0 +1,25 @@
+class Solution {
+    public int carFleet(int target, int[] position, int[] speed) {
+        int[] dist = new int[position.length];
+        int[][] pair = new int[position.length][2];
+
+        for(int i=0;i<position.length;i++){
+            pair[i][0] = position[i];
+            pair[i][1] = speed[i];
+        }
+
+        Arrays.sort(pair,(a,b) -> Integer.compare(b[0],a[0]));
+        double maxTime = 0.0;
+        int fleets=0;
+
+        for(int i=0;i<pair.length;i++){
+            double time = (double)(target - pair[i][0])/pair[i][1];
+            if(time>maxTime){
+                fleets++;
+                maxTime = time;
+            }
+        }
+
+        return fleets;
+    }
+}
